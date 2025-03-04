abe538c12d6b3c57e07d4bfa796fcff468a5176a
AndreiPukhlov <159858829+AndreiPukhlov@users.noreply.github.com>
2025-03-03 19:54:56 -0800

    implicitly wait added

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,10 @@
-import time
 
 from selenium import webdriver
 
 
 def get_driver():
     driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
     return driver
 
 
@@ -17,7 +17,6 @@ class TestLogin:
     def test(self):
         driver = get_driver()
         driver.get(login_page_url)
-        time.sleep(3)
         assert driver.title == 'OrangeHRM'
 
     def test_username_required_field(self):
@@ -25,14 +24,12 @@ class TestLogin:
         # 1.Go to login page Application Under Test
         driver.get(login_page_url)
         # 2. Input valid password in a "password" field. (Password: admin123)
-        time.sleep(2)
         driver.find_element("css selector", '[name="password"]').send_keys('admin123')
         # 3. click Login button
         driver.find_element("css selector", '[type="submit"]').click()
         # Expected result
         # Authorization denied A hint "username field" is required is appear on a screen
         assert driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-        #todo add description to step
+        # todo add description to step
         expected_message = driver.find_element("xpath", "//span[text()='Required']").text
         assert expected_message == 'Required'
-
