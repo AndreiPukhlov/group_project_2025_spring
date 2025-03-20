b19fe40245eb9dffb634111d1f134d74ca42f000
tamarayaz <tamara90gomel@gmail.com>
2025-03-20 11:25:01 -0600

    fixed test_forgot_your_password_cancel

--- tests/ui_tests/test_forgot_password.py
+++ tests/ui_tests/test_forgot_password.py
@@ -31,18 +31,14 @@ class TestForgotPassword:
         message = page.element_is_visible(fp_locators.SUCCESS_MESSAGE).text
         assert message == "Reset Password link sent successfully"
 
-    @pytest.mark.skip(reason="This feature is not ready yet")
-
+    @pytest.mark.regression
     def test_forgot_your_password_cancel(self, driver):
-        # 1. Open the login page
-        driver.get(url.LOGIN_URL)
-        # 2. Click on the 'Forgot your password?' link
-        driver.find_element("xpath", "//p[text()='Forgot your password? ']").click()
-        # 3. Enter an existing username in the 'Username' field
-        driver.find_element("css selector", '[name="username"]').send_keys(data.ADMIN_NAME)
-        # 4. Click the 'Cancel' button
-        driver.find_element("xpath", "//button[text()=' Cancel ']").click()
-
-        # Expected result
-        # The user is returned to the login page
+        # создаем представителя класса LoginPage
+        page = LoginPage(driver, url.LOGIN_URL)
+        page.open()
+        page.element_is_visible(fp_locators.FORGOT_PASSWORT_LINK).click()
+        # создаем представителя класса ForgotPasswordPage
+        page = ForgotPasswordPage(driver, "")
+        page.element_is_visible(fp_locators.USERNAME_FIELD).send_keys(data.ADMIN_NAME)
+        page.element_is_visible(fp_locators.CANCEL_BUTTON).click()
         assert driver.current_url == url.LOGIN_URL
