fde1f2566e0f38928f9af4b89353ea0e337a477a
AndreiPukhlov <159858829+AndreiPukhlov@users.noreply.github.com>
2025-03-21 08:52:29 -0700

    changes to dbs

--- python_themes/decorators.py
+++ python_themes/decorators.py
@@ -2,44 +2,70 @@
 import time
 
 
-def fancy_decorator(func):
+def timer(func):
     def wrapper():
-        print("✨ Fancy Start ✨")
-        print("✨" * 8)
+        start = time.time()
         func()
-        print("✨" * 8)
-        print("✨ Fancy End ✨")
+        end = time.time()
+        result = end - start
+        if result < 60:
+            if result < 2:
+                print(f"It takes {round(result)} second")
+            else:
+                print(f"It takes {round(result)} seconds")
+
+        else:
+            if result < 120:
+                print(f"It takes {round(result / 60)} minute and {round(result % 60)} seconds")
+            else:
+                print(f"It takes {round(result / 60)} minutes and {round(result % 60)} seconds")
 
     return wrapper
 
 
+def fancy_decorator(func):
+    def wrapper():
+        print("✨ Салат ✨")
+        print("✨" * 8)
+        func()
+        print("✨" * 8)
+        print("✨ Одна порция ✨")
 
-def add():
-    print(f"Result is: {1 + 12}")
-
-
-
-def say_hello():
-    print("Hello!")
-
-
-
-def say_hi():
-    print('Hi!')
+    return wrapper
 
 
+@fancy_decorator
+def seld_pod_shuboy():
+    print("Сельдь под шубой")
 
-def say_hey():
-    print("Hey!")
+@fancy_decorator
+def olivie():
+    print("Оливье!")
+
+@timer
+@fancy_decorator
+def mimoza():
+    print('Мимоза')
+
+@fancy_decorator
+def vesenniy():
+    print("Весенний!")
 
 
-say_hi()
-print()
-say_hey()
-print()
-say_hello()
-print()
-add()
+#
+#
+# seld_pod_shuboy()
+# print()
+# olivie()
+# print()
+# vesenniy()
+# print()
+mimoza()
 
 
+@fancy_decorator
+@timer
+def sleeeeep():
+    time.sleep(1)
 
+# sleeeeep()
